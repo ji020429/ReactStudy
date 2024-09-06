397e355ad6396653fab06ac73e9c9e687829d65d
jsy <94359749+ji020429@users.noreply.github.com>
2024-09-06 16:04:44 +0900

    perf: useMemo

--- TodoList/src/components/TodoList.jsx
+++ TodoList/src/components/TodoList.jsx
@@ -1,6 +1,6 @@
 import './TodoList.css';
 import TodoItem from './TodoItem';
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 
 export default function TodoList({todos, onUpdate, onDelete}){
 
@@ -8,18 +8,34 @@ export default function TodoList({todos, onUpdate, onDelete}){
 
     const onChangeSearch = (e) => {
         setSearch(e.target.value);
-    }
+    };
 
     const filterTodos = () => {
-        if(search === ""){
+        if (search === "") {
             return todos;
         }
-        return todos.filter((todo)=>todo.content.toLowerCase().includes(search.toLowerCase())) 
-    }
+        return todos.filter((todo) => todo.content.toLowerCase().includes(search.toLowerCase()));
+    };
+
+    const { totalCount, doneCount, notDoneCount } = useMemo(() => {
+        const totalCount = todos.length;
+        const doneCount = todos.filter((todo) => (todo.isDone)).length;
+        const notDoneCount = totalCount - doneCount;
+        return {
+            totalCount,
+            doneCount,
+            notDoneCount,
+        };
+    }, [todos]);  // todos 값이 변경되었을 때, 콜백 함수가 다시 실행
 
     return(
         <div className='TodoList'>
             <h4>Todos</h4>
+            <div>
+                <div>전체 투두 : {totalCount}</div>
+                <div>완료 투두 : {doneCount}</div>
+                <div>미완료 투두 : {notDoneCount}</div>
+            </div>
             <input 
                 value={search}
                 onChange={onChangeSearch}
