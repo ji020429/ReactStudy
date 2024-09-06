4f2ca0f9e28af4132cbe0c375cace4ab9e0745e6
jsy <94359749+ji020429@users.noreply.github.com>
2024-09-06 15:15:54 +0900

    perf: useState -> useReducer

--- TodoList/src/App.jsx
+++ TodoList/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import Header from './components/Header';
 import TodoEditor from './components/TodoEditor';
 import TodoList from './components/TodoList';
-import { useState, useRef } from 'react';
+import { useState, useRef, useReducer } from 'react';
 
 const mockData = [
   {
@@ -23,35 +23,51 @@ const mockData = [
     content: "음악 공부하기",
     createdDate: new Date().getTime(),
   },
-]
+];
+
+function reducer(state, action) {
+  switch(action.type){
+    case "CREATE":{
+      return [...state, action.data];  // reducer가 반환하는 값으로 state가 변경
+    }
+    case "UPDATE":{
+      return state.map((item)=>item.id === action.data? {...item, isDone: !item.isDone}:item);
+    }
+    case "DELETE":{
+      return state.filter((item)=>(item.id !== action.data));
+    }
+  }
+}
 
 function App() {
 
-  const [todos, setTodos] = useState(mockData);  // 데이터 보관
+  const [todos, dispatch] = useReducer(reducer, mockData);
   const ifRef = useRef(3);
 
   const onCreate = (content) => {  // content: input 폼에 입력한 값
-    const newTodo = {
-      id : ifRef.current++,
-      isDone: false,
-      content,  // 변수의 이름으로 프로퍼티 키 설정하고 값이 할당됨
-      createdDate : new Date().getTime()
-    };
-    setTodos(
-      [...todos, newTodo]
-    );
+    dispatch({
+      type: "CREATE",
+      data: {
+        id : ifRef.current++,
+        isDone: false,
+        content,  // 변수의 이름으로 프로퍼티 키 설정하고 값이 할당됨
+        createdDate : new Date().getTime()
+      },
+    });
   };
 
   const onUpdate = (targetId) => {
-    setTodos(
-      todos.map((todo)=> todo.id === targetId? {...todo, isDone : !todo.isDone} : todo)
-      )
+    dispatch({
+      type: "UPDATE",
+      data: targetId
+    });
   }
 
   const onDelete = (targetId) => {
-    setTodos(
-      todos.filter((todo)=> todo.id !== targetId)
-    );
+    dispatch({
+      type: "DELETE",
+      data: targetId
+    });
   }
 
   return (
