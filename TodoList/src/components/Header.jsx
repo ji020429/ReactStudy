f30453f5c17a27f2882d3c44d04cd1ce8f21e8b2
jsy <94359749+ji020429@users.noreply.github.com>
2024-09-06 16:52:55 +0900

    perf: React.memo

--- TodoList/src/components/Header.jsx
+++ TodoList/src/components/Header.jsx
@@ -1,9 +1,12 @@
 import './Header.css';
+import { memo } from 'react';
 
-export default function Header(){
-    return(
+function Header(){
+    return (
         <div className='Header'>
             <h1>{new Date().toDateString()}</h1>
         </div>
-    )
+    );
 }
+
+export default memo(Header); // memo는 컴포넌트를 받아 최적화된 컴포넌트로 반환
